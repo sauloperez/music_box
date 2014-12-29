b0ed113305ad39e782dd7dbb5f98f59da6044ab0
Pau Pérez <saulopefa@gmail.com>
2014-12-29 14:03:19 +0100

    Encapsulate logic in Player class

--- music_box.rb
+++ music_box.rb
@@ -1,16 +1,37 @@
 #!/usr/bin/env ruby
 
+require 'byebug'
 require 'open3'
+
+class StreamPlayer
+  def initialize(file)
+    @file = file
+  end
+
+  def play
+    spawn_player do |stdin, stdout, stderr|
+      basename = File.basename(@file.path)
+      puts "Playing #{basename}..."
+
+      stdin.puts @file.read
+      stdin.close
+
+      while output = stdout.read do
+        puts output
+      end
+    end
+  end
+
+  private
+
+  def spawn_player
+    Open3.popen3('mpg123', '-') do |stdin, stdout, stderr|
+      yield stdin, stdout, stderr
+    end
+  end
+end
 
 filename = ARGV[0]
 
-Open3.popen3('mpg123', '-') do |stdin, stdout, stderr|
-  puts "Playing #{filename}..."
-
-  stdin.puts File.open(filename).read
-  stdin.close
-
-  while output = stdout.read do
-    puts output
-  end
-end
+track = File.open(filename)
+Player.new(track).play
